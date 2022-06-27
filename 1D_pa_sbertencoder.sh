70a6dbf49f7d6019c44cd8455cf41f48ef41262c
shruti <shrutis1051@gmail.com>
2022-06-27 18:38:05 +0530

    pa encoder script sbert

---
+++ 1D_pa_sbertencoder.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#SBATCH -N 1
+#SBATCH --ntasks-per-node=48
+#SBATCH --time=23:59:00
+#SBATCH --job-name=sbertenc
+#SBATCH --error=./pa_logs/sbertenc_%j.err
+#SBATCH --output=./pa_logs/sbertenc_%j.log
+#SBATCH --partition=small
+cd $SLURM_SUBMIT_DIR 
+
+pwd; hostname; date
+conda activate scidocs
+python 1D_embed_sbert.py
+
+date
